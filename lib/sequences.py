0b67d8e47a243a82925644da136b4fc688caeb2d
student <kmac.kimaru93@gmail.com>
2024-05-28 22:21:33 +0300

    adding sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+    
+    while len(sequence) < length:
+        next = sequence[-1] + sequence[-2]
+        sequence.append(next)
+    print(sequence) 
+
+print_fibonacci(9)
